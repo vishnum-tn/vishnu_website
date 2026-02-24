b62c40f9292327320606db2280da38bba857345f
Vishnu Ms <vishnums775@gmail.com>
2026-02-24 23:59:02 +0530

    vishnu website

--- functions/function.py
+++
@@ -1,9 +0,0 @@
-def greet():
-    print(" Python!")
-
-greet()
-
-print("Hello", end="")
-greet()     
-print("Hello", end=" ")
-
---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+console.log('Portfolio website loaded!');
